288f60680308a86f51edd7d7aebb700161ca6995
George Petasis <petasis@iit.demokritos.gr>
2022-12-10 20:19:49 +0200

    Drop target inside a frame in demos/simple_target.tcl

--- demos/simple_target.tcl
+++ demos/simple_target.tcl
@@ -1,16 +1,18 @@
 package require tkdnd
 catch {console show}
 
-pack [ttk::button .drop_target \
+pack [ttk::frame .frame] -fill both -expand 1
+set drop_target .frame.drop_target
+pack [ttk::button $drop_target \
       -text " Drop Target (I can accept anything!)\
            \n          but only on the left half!"] \
       -fill x -padx 20 -pady 20
 
-tkdnd::drop_target register .drop_target *
+tkdnd::drop_target register $drop_target *
 
 ## Visual feedback:
-bind .drop_target <<DropEnter>> {%W state  active}
-bind .drop_target <<DropLeave>> {%W state !active}
+bind $drop_target <<DropEnter>> {%W state  active}
+bind $drop_target <<DropLeave>> {%W state !active}
 
 ## Position events:
 proc handle_position {widget mouse_x mouse_y drag_source_actions buttons} {
@@ -29,30 +31,30 @@ proc handle_position {widget mouse_x mouse_y drag_source_actions buttons} {
     return refuse_drop
   }
 };# handle_position
-bind .drop_target <<DropPosition>> [list handle_position %W %X %Y %a %b]
+bind $drop_target <<DropPosition>> [list handle_position %W %X %Y %a %b]
 
 ## Drop callbacks:
-bind .drop_target <<Drop>>           {
+bind $drop_target <<Drop>>           {
   puts "Generic data drop: \"%D\""
   %W state !active
   return %A
 }
-bind .drop_target <<Drop:DND_Text>>  {
+bind $drop_target <<Drop:DND_Text>>  {
   puts "Dropped text:  \"%D\""
   %W state !active
   return %A
 }
-bind .drop_target <<Drop:DND_Files>> {
+bind $drop_target <<Drop:DND_Files>> {
   puts "Dropped files: \"[join %D {, }]\""
   %W state !active
   return %A
 }
-bind .drop_target <<Drop:DND_HTML>> {
+bind $drop_target <<Drop:DND_HTML>> {
   puts "Dropped HTML: \"[join %D {, }]\""
   %W state !active
   return %A
 }
-bind .drop_target <<Drop:DND_Color>> {
+bind $drop_target <<Drop:DND_Color>> {
   puts "Dropped color: \"%D\""
   %W state !active
   return %A
